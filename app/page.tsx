11713821331fe606199e201789e9d48af4d2998a
cieko <rahulsaxena2021@hotmail.com>
2024-03-08 00:45:52 +0530

    Setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="text-yellow-700">
+      hey
+      <Button variant={"theme"}>Click here</Button>
+    </main>
+  );
+}
