93e0e3a32ccac0ae9d4aaf42c07903894b4ae074
cieko <rahulsaxena2021@hotmail.com>
2024-03-08 21:21:31 +0530

    initial modal setup

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import InitialModal from "@/components/modals/initial-modal";
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+import React from "react";
 
 const SetupPage = async () => {
-  return (
-    <div>SetupPage</div>
-  )
-}
+  const profile = await initialProfile();
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
 
-export default SetupPage
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return <InitialModal />;
+};
+
+export default SetupPage;
